fba86142fc221b9468e567de064ead03ead8c665
Muralikrishna94 <murali108krishna@gmail.com>
2022-06-16 17:24:01 +0530

    Demo

---
+++ tests/execution.js
@@ -0,0 +1,15 @@
+const puppeteer = require('puppeteer');
+
+describe('My first puppeteer test ', () => {
+  it('Launch Demo website', async function () {
+
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto('https://opensource-demo.orangehrmlive.com/');
+    await page.waitForTimeout(2500);
+    await browser.close();
+
+
+  })
+
+})
